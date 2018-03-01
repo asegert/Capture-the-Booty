165732f92707530552d22bd46a2b8d2dbe0565fc
asegert <zannagal@live.ca>
2018-03-01 11:33:26 -0800

    Item Commit
    Created item prefab and have the board displaying with input.

---
+++ js/prefabs/Item.js
@@ -0,0 +1,61 @@
+//Sets up items
+var Merge = Merge || {};
+
+
+Merge.Item = function(state) {
+     //Intalizes state side data locally
+     this.state = state;
+     this.game = state.game; 
+     
+     Merge.Item.prototype.init = function(texture)
+     {
+         this.texture = texture;
+         this.next = this.getNext(texture);
+         this.sprite = null;
+         
+         return this;
+     };
+    Merge.Item.prototype.getNext = function(texture)
+    {
+        let nextArray = ['coinG', 'coinS', 'coinStackG', 'coinStackS', 'chest', 'gold', 'citrine', 'topaz', 'ruby', 'sapphire', 'emerald', 'amethyst', 'garnet', 'onyx', 'diamond'];
+        
+        for(let i = 0, len = nextArray.length; i<len; i++)
+        {
+            if(nextArray[i]===texture)
+            {
+                return nextArray[i+1];
+            }
+        }
+    }
+    Merge.Item.prototype.setSprite = function(x, y, texture)
+    {
+        this.sprite = this.state.add.sprite(x, y, texture);
+        this.sprite.inputEnabled = true;
+        this.sprite.events.onInputDown.add(function()
+        {
+            console.log('k');
+            //check merge
+        }, this);
+    }
+    Merge.Item.prototype.checkMerge = function(myItems)
+    {
+        let myItemCount = 0;
+        
+        for(let i=0, len=myItems.length; i<len; i++)
+        {
+            if(myItems[i].texture === this.texture)
+            {
+                myItemCount++;
+            }
+        }
+        
+        if(myItemCount > 1)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
--- js/states/Game.js
+++ js/states/Game.js
@@ -3,7 +3,49 @@ var GameName = GameName || {};
 GameName.GameState = {
     create: function ()
     {
+        this.board = [
+                        ['coinG', 'topaz', 'citrine', 'coinS'], 
+                        ['gold', 'onyx', 'coinS', 'onyx'], 
+                        ['coinStackG', 'chest', 'diamond', 'topaz'], 
+                        ['garnet', 'chest', 'sapphire', 'coinStackS']
+                     ];
+        this.myItems = ['coinG', 'coinS', 'coinG'];
         
+        this.board = this.createItems(this.board);
+        this.myItems = this.createItems(this.myItems);
+        this.createBoard(this.board);
+    },
+    createItems(array)
+    {
+        for(let i=0, len=array.length; i<len; i++)
+        {
+            //If it is a 2D array
+            if(Array.isArray(array[i]))
+            {
+                for(let j=0, len2=array[i].length; j<len2; j++)
+                {
+                    let Item = new Merge.Item(this);
+                    array[i][j] = Item.init(array[i][j]);
+                }
+            }
+            else
+            {
+                let Item = new Merge.Item(this);
+                array[i] = Item.init(array[i]);
+            }
+        }
+        return array;
+    },
+    createBoard(board)
+    {
+        for(let i=0, len1=board.length; i<len1; i++)
+        {
+            for(let j=0, len2=board[i].length; j<len2; j++)
+            {
+                board[i][j] = board[i][j].setSprite(35 * i, 35 * j, board[i][j].texture);
+            }
+        }
+        return board;
     },
     update: function ()
     {
